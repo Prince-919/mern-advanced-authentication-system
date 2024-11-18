99425fb315d728250b6c9a514735d99436f7d07e
Prince Sharma <86801714+Prince-919@users.noreply.github.com>
2024-11-18 21:40:38 +0530

    final commit

--- client/src/components/NorFound.jsx
+++ client/src/components/NorFound.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen">
       <div className="text-center">
         <h1 className="text-6xl font-bold text-red-500">404</h1>
-        <p className="text-2xl text-gray-600 mt-4">
+        <p className="text-2xl text-gray-300 mt-4">
           Oops! The page you're looking for doesn't exist.
         </p>
         <Link
